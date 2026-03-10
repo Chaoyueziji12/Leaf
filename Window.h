601b6dcf91f6593fd014202cf6563c32fd4925b8
Chaoyueziji12 <1214694469@qq.com>
2026-03-10 21:10:26 +0800

    Added a argument to edit the window update delay and added a Leaf engine logo

--- Window.h
+++ Window.h
@@ -3,12 +3,14 @@
 
 typedef SDL_Window* LeafWindow;
 const int LF_WINDOWPOS_CENTERED = SDL_WINDOWPOS_CENTERED;
+int windowDelay = 10;
 
 namespace Leaf {
-	LeafWindow CreateWindow(const char* title, int width, int height, int posx, int posy)
+	LeafWindow CreateWindow(const char* title, int width, int height, int posx, int posy, const int delay=10)
 	{
 		SDL_Init(SDL_INIT_VIDEO);
 		SDL_Window* window = SDL_CreateWindow(title, posx, posy, width, height, SDL_WINDOW_SHOWN);
+		windowDelay = delay;
 		return (LeafWindow)window;
 	}
 	void RunWindow(LeafWindow window)
@@ -20,7 +22,7 @@ namespace Leaf {
 			{
 				break;
 			}
-			SDL_Delay(10);
+			SDL_Delay(windowDelay);
 		}
 		SDL_DestroyWindow(window);
 		SDL_Quit();
